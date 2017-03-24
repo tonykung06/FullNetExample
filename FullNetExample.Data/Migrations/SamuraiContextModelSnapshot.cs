ff4f0ec4a24aa1736a8c5154dcb7213d9d02d1bd
tonykung <tonykung06@gmail.com>
2017-03-25 02:03:23 +0800

    initial commit

---
+++ FullNetExample.Data/Migrations/SamuraiContextModelSnapshot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using FullNetExample.Data;
+
+namespace FullNetExample.Data.Migrations
+{
+    [DbContext(typeof(SamuraiContext))]
+    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("FullNetExample.Domain.Battle", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("EndDate");
+
+                    b.Property<string>("Name");
+
+                    b.Property<DateTime>("StartDate");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Battles");
+                });
+
+            modelBuilder.Entity("FullNetExample.Domain.Quote", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("SamuraiId");
+
+                    b.Property<string>("Text");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("SamuraiId");
+
+                    b.ToTable("Quotes");
+                });
+
+            modelBuilder.Entity("FullNetExample.Domain.Samurai", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BattleId");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("BattleId");
+
+                    b.ToTable("Samurais");
+                });
+
+            modelBuilder.Entity("FullNetExample.Domain.Quote", b =>
+                {
+                    b.HasOne("FullNetExample.Domain.Samurai", "Samurai")
+                        .WithMany("Quotes")
+                        .HasForeignKey("SamuraiId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("FullNetExample.Domain.Samurai", b =>
+                {
+                    b.HasOne("FullNetExample.Domain.Battle")
+                        .WithMany("Samurais")
+                        .HasForeignKey("BattleId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ FullNetExample.Data/SamuraiContext.cs
@@ -0,0 +1,25 @@
+﻿using FullNetExample.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullNetExample.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server = (localdb)\\mssqllocaldb; Database = SamuraiData; Trusted_Connection = True; "    
+            );
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
